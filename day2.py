c2956b68497781a807a2c9238a83ab825b788e74
Santhosh563 <santhoshp4399@gmail.com>
2025-02-22 18:38:26 +0530

    second commit

---
+++ day2.py
@@ -0,0 +1,60 @@
+"""
+In python,Arithmetic functions and Logical functions (operators)
+
+#1.Arithmetic operator ==> (+,-,/,%,*) --> mathematical operators
+
++ ==> addition 5+3=8
+- ==> subtraction 5-3=2
+* ==> multiplication 6*3=18
+/ ==> division (float)
+// ==> floor division (int)
+% ==> Modulus (remainder)
+** ==> exponentiation (power)
+
+#2.Logical operator ==> and, or, not
+and ==> returns true if both condition are True
+or ==> returns true if atleast one condition is True
+not ==> returns opposite of the condition
+"""
+
+# a=5
+# b=3
+
+# result=a%b
+# print(result)
+# a=5
+# b=10
+
+# print(a>3 and b>8) #output=true
+# print(a>3 or b<8)  #output=false
+# print(not(a>3))
+# print(not(a<3))
+
+#3.comparision operator
+'''
+==
+|=
+>
+<
+>=
+<=
+'''
+
+x=10
+y=5
+
+print(x==y) #False
+print(x|y)  #True
+print(x>y)  #True
+print(x<y)  #False
+print(x>=y) #True
+print(x<=y) #False
+
+#4.by using inbuilt functions or operators by math
+import math
+
+pooja=100
+
+print(math.sqrt(144))
+
+
